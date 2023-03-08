be1bebb3bd2291533169144903d00de92039f176
martinskauan <kauanmartinsdapoca@gmail.com>
2023-03-08 10:47:37 -0300

    Aulaum

---
+++ algoritimo.js
@@ -0,0 +1,14 @@
+let numeroUm =0;
+let numeroDois =10;
+let numeroTres = 20;
+
+
+//diferença entre let e var (diferença de espaço) 
+// a var é um escopo global que pega no documento inteiro e o let só pega dentro da chave.
+numeroUm= numeroDois;
+console.log (numeroUm)
+numeroUm= numeroTres;
+console.log (numeroUm);
+numeroUm=numeroUm
+//console.log serve pra  Imprime o texto como uma mensagem de log
+
---
+++ let-var-const.js
@@ -0,0 +1,10 @@
+if (true){
+
+
+     var a = 10;
+    console.log(a);
+
+}
+//console.log (a) fora do espaço vai da erro 
+// a diferença entre let e var é que a let é escopo local só pega oq está dentro da chave.
+// a var pega o documento inteiro. 
