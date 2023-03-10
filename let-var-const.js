5f3866e4c80933e8949db14c945e68749002e812
martinskauan <kauanmartinsdapoca@gmail.com>
2023-03-10 09:48:58 -0300

    oficina2

--- let-var-const.js
+++ let-var-const.js
@@ -1,10 +1,13 @@
-if (true){
+let numeroUm =0;
+let numeroDois =10;
+let numeroTres = 20;
 
 
-     var a = 10;
-    console.log(a);
-
-}
-//console.log (a) fora do espaço vai da erro 
-// a diferença entre let e var é que a let é escopo local só pega oq está dentro da chave.
-// a var pega o documento inteiro. 
+//diferença entre let e var (diferença de espaço) 
+// a var é um escopo global que pega no documento inteiro e o let só pega dentro da chave.
+numeroUm= numeroDois;
+console.log (numeroUm)
+numeroUm= numeroTres;
+console.log (numeroUm);
+numeroUm=numeroUm
+//console.log serve pra  Imprime o texto como uma mensagem de log
---
+++ poema.js
@@ -0,0 +1,18 @@
+let TemDinheiro = true;
+let temSol= false;
+
+if (TemDinheiro) {
+    if (temSol){
+        console.log("guaruja");
+    } else {
+        console.log("restaurante");
+    }
+} else { 
+    if (Temsol) 
+    console.log("ibirapuera");
+  else { 
+     console.log("aula");
+  }
+}
+  
+    
